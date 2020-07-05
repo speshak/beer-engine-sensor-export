8a7eae1b84ea2e33e7f2c26668ad80e655bb49c5
Scott Peshak <scott@peshak.net>
2020-07-05 14:03:05 -0500

    Send logger to stdout

--- export_sensors.py
+++ export_sensors.py
@@ -4,9 +4,14 @@ import logging
 import requests
 import tempfile
 import os
+import sys
 import boto3
 
 logger = logging.getLogger()
+out_hdlr = logging.StreamHandler(sys.stdout)
+out_hdlr.setFormatter(logging.Formatter('%(asctime)s %(message)s'))
+out_hdlr.setLevel(logging.INFO)
+logger.addHandler(out_hdlr)
 logger.setLevel(logging.INFO)
 
 s3_client = boto3.client('s3')
