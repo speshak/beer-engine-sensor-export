be60fb550fb8c7aadc6a1520375c3c5580377250
Scott Peshak <scott@peshak.net>
2020-07-05 19:40:27 -0500

    Allow running as command or as an activity

--- export_sensors.py
+++ export_sensors.py
@@ -6,6 +6,8 @@ import tempfile
 import os
 import sys
 import boto3
+from datetime import datetime
+from datetime import date
 
 logger = logging.getLogger()
 out_hdlr = logging.StreamHandler(sys.stdout)
@@ -18,22 +20,40 @@ s3_client = boto3.client('s3')
 GRAPH_HOST = os.environ['GRAPHITE_HOST']
 
 
-def export_sensors(**task_input):
-    ferm_id = task_input['fermenter_id']
-    start = task_input['start']
-    end = task_input['end']
+def session_info(session_id):
+    logger.info("Getting session metadata from info service")
+    api = os.environ['INFO_API']
+    res = requests.get(f"{api}sessions/{session_id}")
+    res.raise_for_status()
 
-    logger.info(f"Fetching sensor data for {ferm_id} ({start} - {end})")
+    data = res.json()
+
+    ret = {
+            'ferm_id': data['fermenter'],
+            'start': date.fromisoformat(data['brew_date']).strftime('%Y%m%d'),
+        }
+
+    ret['end'] = data.get('package_date', datetime.now().strftime('%Y%m%d'))
+
+    return ret
+
+
+def export_sensors(**task_input):
+    info = session_info(task_input['session_id'])
+
+    logger.info(f"Fetching sensor data for {info['ferm_id']} "
+                f"({info['start']} - {info['end']})")
     url = (
             f"{GRAPH_HOST}/render/?target="
-            f"stats.gauges.cbpi.fermenter.{ferm_id}.*"
-            f"&from={start}&until={end}"
+            f"stats.gauges.cbpi.fermenter.{info['ferm_id']}.*"
+            f"&from={info['start']}&until={info['end']}"
             "&format=json"
         )
 
     res = requests.get(url, stream=True)
     res.raise_for_status()
-    key = f"sensors/{ferm_id}-{start}-{end}.json"
+    key = (f"sensors/{task_input['session_id']}/"
+           f"{info['ferm_id']}-{info['start']}-{info['end']}.json")
 
     with tempfile.TemporaryFile() as f:
         for block in res.iter_content(1024):
@@ -49,8 +69,15 @@ def export_sensors(**task_input):
         logger.info(f"Wrote {key}")
 
 
-if __name__ == '__main__':
+def run_as_worker():
     activity_arn = os.environ['ACTIVITY_ARN']
     logger.info(f"Starting worker for {activity_arn}")
     worker = ActivityWorker(activity_arn, export_sensors)
     worker.listen()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        export_sensors(session_id=sys.argv[1])
+    else:
+        run_as_worker()
